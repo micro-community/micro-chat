19ba018fb27cee8ec70e8cefc0fd91f1fc31b382
Edward <crazybber@outlook.com>
2020-10-25 16:47:15 +0800

    to github.com/micro/micro/v3/service/context/metadata

--- handler/chat_connect.go
+++ handler/chat_connect.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	pb "github.com/micro-community/micro-chat/proto"
-	"github.com/micro/go-micro/metadata"
+	"github.com/micro/micro/v3/service/context/metadata"
 	"github.com/micro/micro/v3/service/errors"
 	"github.com/micro/micro/v3/service/events"
 	"github.com/micro/micro/v3/service/logger"
